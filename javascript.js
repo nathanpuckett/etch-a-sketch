de8c9a6ac282adbe768496b1aa1a9a684f97046b
nathanpuckett <nathanfpuckett@gmail.com>
2022-12-03 15:22:07 -0600

    Create 16x16 grid of divs

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Create a 16x16 grid of square divs
+
+const gridContainer = document.querySelector('.grid-container');
+
+for (let i = 1; i <= 16; i++) {
+  const gridRow = document.createElement('div');
+  gridRow.classList.add(`grid-row`);
+  gridContainer.appendChild(gridRow);
+
+  for (let j = 1; j <= 16; j++) {
+    const gridColumn = document.createElement('div');
+    gridColumn.classList.add(`grid-column`);
+    gridColumn.textContent = `${j}`;
+    gridRow.appendChild(gridColumn);
+  }
+}
