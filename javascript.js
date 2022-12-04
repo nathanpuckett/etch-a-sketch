ba522527c0c0b3caae0c2088ce6465e4ddb6e2e9
nathanpuckett <nathanfpuckett@gmail.com>
2022-12-03 18:17:04 -0600

    Add 'Reset' button

--- javascript.js
+++ javascript.js
@@ -1,31 +1,53 @@
-// Create a 16x16 grid of square divs
-
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 1; i <= 16; i++) {
-  const gridRow = document.createElement('div');
-  gridRow.classList.add(`grid-row`);
-  gridContainer.appendChild(gridRow);
+function createGrid(resolution) {
+  for (let i = 1; i <= resolution; i++) {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add(`grid-row`);
+    gridContainer.appendChild(gridRow);
+  
+    for (let j = 1; j <= resolution; j++) {
+      const gridColumn = document.createElement('div');
+      gridColumn.classList.add(`grid-column`);
+      // gridColumn.textContent = `${i}`;
+      gridRow.appendChild(gridColumn);
+    }
+  }  
+};
 
-  for (let j = 1; j <= 16; j++) {
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add(`grid-column`);
-    gridColumn.textContent = `${j}`;
-    gridRow.appendChild(gridColumn);
+function hover() {
+  const hoverSquares = document.querySelectorAll('.grid-column');
+  hoverSquares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      square.classList.add('grid-hover');
+    });
+    square.addEventListener('mouseout', () => {
+      square.classList.remove('grid-hover');
+    });
+  });
+};
+
+createGrid(16);
+hover();
+
+function clearGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
-}
+};
 
-const hoverSquares = document.querySelectorAll('.grid-column');
-
-hoverSquares.forEach(square => {
-  square.addEventListener('mouseover', () => {
-    square.classList.add('grid-hover');
-  });
-  
-  square.addEventListener('mouseout', () => {
-    square.classList.remove('grid-hover');
-  });
-  
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+  clearGrid();
+  const resolution = prompt('number of squares per side?');
+  if (resolution <= 100) {
+    createGrid(resolution);
+  } else {
+    createGrid(16);
+  }
+  hover();
 });
+
+
 
 
