3ad995c13a77fd30a587cc19e767396680984400
nathanpuckett <nathanfpuckett@gmail.com>
2022-12-04 12:13:36 -0600

    Add 'rainbow mode' & update README

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# etch-a-sketch
-Etch-a-Sketch Project for TOP
-https://nathanpuckett.github.io/etch-a-sketch/
+# Etch A Sketch
 
-This project is an Etch-a-Sketch simulator that demonstrates DOM manipulation, event handling, & flexbox. Enjoy!
-- Nathan Puckett
+This project is an Etch-a-Sketch simulator with a user-controlled resolution from 1x1 to 100x100. It also features an optional rainbow mode that draws in random colors. This project demonstrates DOM manipulation, event handling, & flexbox. You can view the project and interact with it [here.] (https://nathanpuckett.github.io/etch-a-sketch/) Enjoy!
--- javascript.js
+++ javascript.js
@@ -15,11 +15,25 @@ function createGrid(resolution) {
   }  
 };
 
-function hover() {
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+
+function hover(color = 'regular') {
   const hoverSquares = document.querySelectorAll('.grid-column');
   hoverSquares.forEach(square => {
     square.addEventListener('mouseover', () => {
-      square.classList.add('grid-hover');
+      if (color === 'regular') {
+        square.classList.add('grid-hover');
+      } else if (color === 'rainbow') {
+        square.style.backgroundColor = getRandomColor();
+      } else {
+        console.log('ERROR');
+      }
     });
     /*
     square.addEventListener('mouseout', () => {
@@ -38,8 +52,7 @@ function clearGrid() {
   }
 };
 
-const resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', () => {
+function resetGrid() {
   clearGrid();
   const resolution = prompt('How many squares per side? (Max: 100)');
   if (resolution <= 100 && resolution > 0) {
@@ -47,5 +60,17 @@ resetButton.addEventListener('click', () => {
   } else {
     createGrid(100);
   }
+};
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+  resetGrid();
   hover();
+});
+
+
+const rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', () => {
+  resetGrid();
+  hover('rainbow');
 });
